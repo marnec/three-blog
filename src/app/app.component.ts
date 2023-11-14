23f0c5732e82b6a11bf42d9879d173e451cc411e
marnec <marnec@ryzenbeast.localdomain>
2023-11-14 20:11:03 +0100

    try fix asset path setting the same baseHref in dev and prod

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MarkdownModule } from 'ngx-markdown';
+import { OrbitCtrlComponent } from './orbit-ctrl/orbit-ctrl.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MarkdownModule],
+  imports: [CommonModule, RouterOutlet, MarkdownModule, OrbitCtrlComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'three-blog';
 }
